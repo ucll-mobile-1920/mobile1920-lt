8db63ece4bbee9c89843b0142672104223e936ae
TomasAdomavicius <tomadomavicius@gmail.com>
2019-09-22 19:32:53 +0200

    create list of hints

--- sTalker.Shared/Models/Player.cs
+++ sTalker.Shared/Models/Player.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace sTalker.Shared.Models
 {
     public class Player
@@ -8,8 +10,7 @@ namespace sTalker.Shared.Models
             get;
             set;
         }
-        public string hint1;
-        public string hint2;
+        public List<string> hints;
         public int roomCode;
         public bool isAdmin;
         public int points;
@@ -18,6 +19,7 @@ namespace sTalker.Shared.Models
         public Player(bool isAdmin)
         {
             this.isAdmin = isAdmin;
+            hints = new List<string>();
         }
     }
 }
