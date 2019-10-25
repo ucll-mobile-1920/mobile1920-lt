f9019ef98a369dd93c1ebc5f1a8a510c573852d2
Justė Naujokaitytė <justenau@gmail.com>
2019-10-25 22:05:42 +0200

    Fill in hints

--- sTalker.Mobile/Activities/GameActivity.cs
+++ sTalker.Mobile/Activities/GameActivity.cs
@@ -1,24 +1,60 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
-using Android.App;
+﻿using Android.App;
 using Android.OS;
 using Android.Widget;
+using sTalker.Helpers;
+using sTalker.Shared.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace sTalker.Activities
 {
     [Activity(Label = "GameActivity")]
     public class GameActivity : Activity
     {
+        private EditText hint1;
+        private EditText hint2;
+        private EditText hint3;
+        private EditText hint4;
+        private EditText hint5;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.game);
 
+            hint1 = FindViewById<EditText>(Resource.Id.hint1);
+            hint2 = FindViewById<EditText>(Resource.Id.hint2);
+            hint3 = FindViewById<EditText>(Resource.Id.hint3);
+            hint4 = FindViewById<EditText>(Resource.Id.hint4);
+            hint5 = FindViewById<EditText>(Resource.Id.hint5);
+
+
+
 
             FindViewById<Button>(Resource.Id.foundSomeone_btn).Click += (sender, e) => {
                 StartActivity(typeof(ProveItActivity));
             };
+
+            FillPlayerData();
+            
+        }
+
+        public async void FillPlayerData()
+        {
+            var player = await DataHelper.GetFirebase().Child($"Games/{GameInfo.roomCode}/Players/{GameInfo.player.UserId}/playerToFind")
+                .OnceSingleAsync<Player>();
+            GameInfo.player.playerToFind = player;
+            try
+            {
+                hint1.Text = player.hints[0];
+                hint2.Text = player.hints[1];
+                hint3.Text = player.hints[2];
+                hint4.Text = player.hints[3];
+                hint5.Text = player.hints[4];
+            } catch 
+            {
+            }
         }
     }
 }
--- sTalker.Mobile/Helpers/GameHelper.cs
+++ sTalker.Mobile/Helpers/GameHelper.cs
@@ -12,16 +12,15 @@ namespace sTalker.Helpers
         {
             var players = (await DataHelper.GetFirebase().Child($"Games/{roomCode}/Players").
                 OnceAsync<Player>()).Where(x=>!x.Object.isAdmin)
-                .Select(x => new Player { Name = x.Object.Name, RecognitionServiceId = x.Object.RecognitionServiceId, UserId = x.Object.UserId }).ToList();
+                .Select(x => new Player { Name = x.Object.Name, RecognitionServiceId = x.Object.RecognitionServiceId, UserId = x.Object.UserId, hints = x.Object.hints }).ToList();
 
             if (players.Count() < 1)
                 return;
 
             for (int i=1; i<players.Count(); i++)
             {
-                players[i].playerToFind = players[i - 1];
                 await DataHelper.GetFirebase().Child($"Games/{roomCode}/Players/{players[i].UserId}/playerToFind").
-                    PutAsync(players[i].playerToFind);
+                    PutAsync(players[i-1]);
             }
             await DataHelper.GetFirebase().Child($"Games/{roomCode}/Players/{players[0].UserId}/playerToFind").
                     PutAsync(players.Last());
